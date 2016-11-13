245b7eb9cae720148333763355677ab1c06ca2f2
Bruno Coudoin <bruno.coudoin@gcompris.net>
2016-11-13 17:51:59 +0100

    remove the plan from the scribus file
    now concact everything with a script gen.sh

---
+++ programme/gen.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+inkscape --export-pdf plan-enseeiht-cdl.pdf plan-enseeiht-cdl.svg
+pdf270 --suffix 'rotated' plan-enseeiht-cdl.pdf
+#pdf90 --suffix 'rotated' tableau-schedule.pdf (no needed but why?)
+pdfjoin --outfile prog-concat.pdf programme.pdf plan-enseeiht-cdl-rotated.pdf tableau-schedule.pdf
+pdfjam  --outfile prog-booklet.pdf prog-concat.pdf '1,2,3,10,11,12,13,14,4,15,16,5,6,7,8,9'
+rm prog-concat.pdf
+rm plan-enseeiht-cdl.pdf
+rm plan-enseeiht-cdl-rotated.pdf
