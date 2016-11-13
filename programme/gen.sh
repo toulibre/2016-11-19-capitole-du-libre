c080f27d6e5382eae64163d6545c712c57e288c7
Bruno Coudoin <bruno.coudoin@gcompris.net>
2016-11-13 17:56:48 +0100

    now generate the schedule pdf
    added prog-booklet.pdf which is the final result

--- programme/gen.sh
+++ programme/gen.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+unoconv -f pdf tableau-schedule.odt
 inkscape --export-pdf plan-enseeiht-cdl.pdf plan-enseeiht-cdl.svg
 pdf270 --suffix 'rotated' plan-enseeiht-cdl.pdf
 #pdf90 --suffix 'rotated' tableau-schedule.pdf (no needed but why?)
@@ -7,3 +8,4 @@ pdfjam  --outfile prog-booklet.pdf prog-concat.pdf '1,2,3,10,11,12,13,14,4,15,16
 rm prog-concat.pdf
 rm plan-enseeiht-cdl.pdf
 rm plan-enseeiht-cdl-rotated.pdf
+rm tableau-schedule.pdf
